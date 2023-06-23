4bb9d92c7e33496534629a60c945b8a7badb444d
Joshua Ryder <josh@topswagcode.com>
2023-06-23 19:14:15 +0200

    remove default styling

---
+++ ClientApp/src/routes/BottomNavigation.svelte
@@ -0,0 +1,41 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+	import { goto } from '$app/navigation';
+
+	console.log('test');
+
+	const handleClick = (link) => {
+		goto(link);
+	}
+	
+</script>
+
+
+
+<div class="btm-nav"><!--https://flowbite.com/icons/-->
+	<button on:click={() => handleClick("/")} class="{$page.url.pathname === '/' ? 'active' : undefined} hover:bg-gray-50 dark:hover:bg-gray-800">
+		<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+			<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8v10a1 1 0 0 0 1 1h4v-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v5h4a1 1 0 0 0 1-1V8M1 10l9-9 9 9"/>
+		  </svg>
+	  <span class="btm-nav-label">Home</span>
+	</button>
+	<button on:click={() => handleClick("/about")} class="{$page.url.pathname === '/about' ? 'active' : undefined} hover:bg-gray-50 dark:hover:bg-gray-800">
+		<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+			<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 11 14H9a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 10 19Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+		  </svg>
+	  <span class="btm-nav-label">About</span>
+	</button>
+	<button on:click={() => handleClick("/weather")} class="{$page.url.pathname === '/weather' ? 'active' : undefined} hover:bg-gray-50 dark:hover:bg-gray-800">
+	  <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+		<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 3V1m0 18v-2M5.05 5.05 3.636 3.636m12.728 12.728L14.95 14.95M3 10H1m18 0h-2M5.05 14.95l-1.414 1.414M16.364 3.636 14.95 5.05M14 10a4 4 0 1 1-8 0 4 4 0 0 1 8 0Z"/>
+	  </svg>
+	  <span class="btm-nav-label">Weather forecasts</span>
+	</button>
+</div>
+
+
+<style>
+
+</style>
---
+++ ClientApp/src/routes/TopBar.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+	import { goto } from '$app/navigation';
+
+	console.log('test');
+
+	const handleClick = (link) => {
+		goto(link);
+	}
+	
+</script>
+
+
+
+<div class="navbar bg-neutral text-neutral-content">
+	<div class="flex-1">
+	  <a class="btn btn-ghost normal-case text-xl">TopSwagCode</a>
+	</div>
+	<div class="flex-none gap-2">
+	  <div class="form-control">
+		<input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+	  </div>
+	  <div class="dropdown dropdown-end">
+		<label tabindex="0" class="btn btn-ghost btn-circle avatar">
+		  <div class="w-10 rounded-full">
+			<img src="https://topswagcode.com/assets/banner-logo.png" />
+		  </div>
+		</label>
+		<ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+		  <li>
+			<a class="justify-between">
+			  Profile
+			  <span class="badge">New</span>
+			</a>
+		  </li>
+		  <li><a>Settings</a></li>
+		  <li><a>Logout</a></li>
+		</ul>
+	  </div>
+	</div>
+  </div>
+
+<style>
+
+</style>
